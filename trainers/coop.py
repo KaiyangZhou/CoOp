1e5ec09f44c58a271233346ac0a42b0ff8011bdc
kaiyangzhou <k.zhou.vision@gmail.com>
2021-10-11 11:19:08 +0800

    update indent

--- trainers/coop.py
+++ trainers/coop.py
@@ -51,10 +51,7 @@ class TextEncoder(nn.Module):
 
         # x.shape = [batch_size, n_ctx, transformer.width]
         # take features from the eot embedding (eot_token is the highest number in each sequence)
-        x = (
-            x[torch.arange(x.shape[0]), tokenized_prompts.argmax(dim=-1)]
-            @ self.text_projection
-        )
+        x = x[torch.arange(x.shape[0]), tokenized_prompts.argmax(dim=-1)] @ self.text_projection
 
         return x
 
